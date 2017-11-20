6d5c1e3239a1f158171bc97d07bed1264cca9ac3
蔵光駿 <kurakoushun@shuns-macbook.local>
2017-11-20 12:42:39 +0900

    Initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+
+  def top
+  end
+  
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    respond_to do |format|
+      format.html { redirect_to books_path,notice: 'Book was successfully created.' }
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_path, notice: 'Book was successfully destroyed.' }
+    end
+    
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
+end
